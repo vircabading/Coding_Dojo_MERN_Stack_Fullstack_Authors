f75f0616d364af95e28761eff6de061465ac894e
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-26 14:54:57 -0800

    implement mongoose.config

---
+++ server/config/mongoose.config.js
@@ -0,0 +1,15 @@
+/////////////////////////////////////////////////
+//  MONGOOSE CONFIG
+/////////////////////////////////////////////////
+
+// //// FIELDS //////////////////////////////////
+const mongoose = require("mongoose");
+const DATABASE_NAME = "products_db";
+
+// //// CONNECT SERVER TO DATABASE //////////////
+mongoose.connect("mongodb://localhost/" + DATABASE_NAME, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+})
+	.then(() => console.log("🌈🌈🌈 Established a connection to the database 🌈🌈🌈"))
+	.catch(err => console.log(`🤷🤷🤷 Something went wrong when connecting to the database 🤷🤷🤷`, err));
