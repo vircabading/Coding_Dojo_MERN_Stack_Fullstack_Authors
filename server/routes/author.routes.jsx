c20e765019c17ed6087385018e0174ca73cc8b02
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-26 20:54:15 -0800

    create routes

--- server/routes/author.routes.jsx
+++ server/routes/author.routes.jsx
@@ -2,21 +2,22 @@
 //  AUTHOR ROUTES
 /////////////////////////////////////////////////
 
-// //// FIELDS //////////////////////////////////
+//// FIELDS //////////////////////////////////
 const AuthorController = require("../controllers/author.controller");
+const Author = require("../models/author.model");
 
 // //// ROUTES //////////////////////////////////
 module.exports = app => {
     // **** Create ******************************
-    app.post("/api/authors/create", AuthorController.createAuthor )
+    app.post("/api/authors/new", AuthorController.createAuthor )
 
-    // **** Retrieve ****************************
-    app.get("/api/authors", AuthorController.findAllAuthors );
-    app.get("/api/authors/:id", AuthorController.fineOneAuthor );
+    // // **** Retrieve ****************************
+    app.get("/api/authors", AuthorController.findAllAuthors);
+    app.get("/api/authors/:id", AuthorController.fineOneAuthor);
 
-    // **** Update ******************************
-    app.put("/api/authors/:id/update", AuthorController.updateAuthor );
+    // // **** Update ******************************
+    app.put("/api/authors/update/:id", AuthorController.updateAuthor);
 
-    // **** Delete ******************************
-    app.delete("/api/authors/:id/delete", ProductController.deleteProduct);
+    // // **** Delete ******************************
+    app.delete("/api/authors/delete/:id", AuthorController.deleteAuthor);
 }
--- server/server.js
+++ server/server.js
@@ -10,6 +10,13 @@ const PORT = 8000;
 
 // //// MIDDLE-WARE /////////////////////////////
 app.use(express.json(), express.urlencoded({ extended: true }));
-app.use(cors());    // enable Cross Origin Requests in Project
+// app.use(cors());    // enable Cross Origin Requests in Project
 
+// INITIALIZE SERVER CONNECTION TO DATABASE /////
+require("./config/mongoose.config");
+
+// //// IMPLEMENT ROUTES ////////////////////////
+require("./routes/author.routes")(app)
+
+// //// EXECUTE SERVER //////////////////////////
 app.listen(PORT, () => console.log(`🦄🦄🦄 The server is all fired up on port ${PORT} 🦄🦄🦄`));
