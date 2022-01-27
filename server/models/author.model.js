5029527c91b7b16d0314a7747b1dbbbaf6ef8ba8
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-26 16:03:04 -0800

    implement author model

---
+++ server/models/author.model.js
@@ -0,0 +1,21 @@
+/////////////////////////////////////////////////
+//  AUTHOR MODEL
+/////////////////////////////////////////////////
+
+// //// FIELDS //////////////////////////////////
+const mongoose = require("mongoose");
+
+// //// SCHEMAS /////////////////////////////////
+const AuthorSchema = new mongoose.Schema({
+	name: {
+        type: String,
+        required: [true, "PATH" is required"],
+        minlength: [3, "{PATH} must be at least three characters long"]
+    }, 
+}, {timestamps: true});			// Timestamps implement CreatedAt and UpdatedAt
+
+// //// MODELS //////////////////////////////////
+const Author = mongoose.model("Author", AuthorSchema);
+
+// **** Export Model ********
+module.exports = Author;
