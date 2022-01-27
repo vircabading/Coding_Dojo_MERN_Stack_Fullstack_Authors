5cb03d440806e304dc93c03c6e4b5b8d43fdd5ab
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-26 18:44:19 -0800

    implement author.controller in server

--- server/controllers/author.controller.jsx
+++ server/controllers/author.controller.jsx
@@ -25,7 +25,73 @@ module.exports.create = (req, res) => {
 
 // //// RETRIEVE ////////////////////////////////
 
+// **** Find All ********
+module.exports.findAllAuthors = (req, res) => {
+    Author.find()
+        .then( allAuthors => 
+            res.json({
+                authors: allAuthors,
+                message: "🦄🦄🦄 Success: Found All 🦄🦄🦄"
+            })
+        )
+        .catch( err =>
+            res.json({
+                message: "🤚🏼🤚🏼🤚🏼 Failure: UnAble to Find All ✋🏼✋🏼✋🏼",
+                error: err
+            })
+        )
+};
+
+// **** Find One ********
+module.exports.fineOneAuthor = (req, res) => {
+    Author.findById(req.params.id)
+        .then(
+            oneAuthor => res.json ({
+                author: oneAuthor,
+                message: "🌈🌈🌈 Success: Found one 🌈🌈🌈"
+            })
+        )
+        .catch( err =>
+            res.json({
+                message: "🛑🛑🛑 Failure: UnAble to Find a Product 🛑🛑🛑",
+                error: err
+            })
+        )
+};
 
 // //// UPDATE //////////////////////////////////
 
+module.exports.updateAuthor = (req,res) => {
+    Author.findByIdAndUpdate( req.params.id, req.body, 
+        { new: true, runValidators: true })
+        .then(
+            updatedAuthor => res.json({
+                author: updatedAuthor,
+                message: "🍔🍔🍔 Success: Update 🍔🍔🍔"
+            })
+        )
+        .catch( err =>
+            res.json({
+                message: "🛑🛑🛑 Failure: UnAble to Update 🛑🛑🛑",
+                error: err
+            })
+        )
+};
+
 // //// DELETE //////////////////////////////////
+
+module.exports.deleteAuthor = (req, res) => {
+    Author.findByIdAndDelete(req.params.id)
+        .then(
+            result => res.json ({
+                result: result,
+                message: "🍕🍕🍕 Success: Delete  🍕🍕🍕"
+            })
+        )
+        .catch( err =>
+            res.json({
+                message: "🛑🛑🛑 Failure: UnAble to Delete 🛑🛑🛑",
+                error: err
+            })
+        )
+};
